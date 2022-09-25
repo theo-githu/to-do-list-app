a727e46ad4453653f61efaa6e6f79f8fc1f57f4f
theo-githu <111968864+theo-githu@users.noreply.github.com>
2022-09-25 17:31:47 +0100

    debug
    update script src & append

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function newItem() {
     //1. Adding a new item to the list of items: 
     let li = $('<li></li>');
     let inputValue = $('#input').val();
-    li.appendChild(inputValue);
+    li.append(inputValue);
 
     if (inputValue === '') {
         alert("You must write something!");
